8b316a9d918da48a0759b6146aa0f7fd6b98d778
kappsona <kapana124@gmail.com>
2022-09-21 18:33:58 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,16 @@
+name = "giorgi"
+
+age = 23
+surname = "kapanadze"
+height = 196
+knows_programing = False
+is_chad = False
+
+print(name + " " + surname + " " + str(age) + " " + str(height))
+
+print(name)
+print(type(age))
+print(type(height))
+print(type(knows_programing))
+print(type(is_chad))
+
