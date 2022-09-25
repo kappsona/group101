94d905b84afb3f7470711edcf699fb643856a64d
kappsona <kapana124@gmail.com>
2022-09-25 16:40:32 +0400

    day2 commit

---
+++ day2/lesson2.py
@@ -0,0 +1,38 @@
+
+
+from sys import get_coroutine_origin_tracking_depth
+
+
+name = "giorgi"
+age = 20
+height = 196
+
+
+print
+
+
+# print("chemi saxelia {} da chemi asakia {} chemi simaglea {}" .format(name, age, height))
+
+full_name="giorgi kapanadze"
+
+# print(full_name[7])
+#print(full_name[2:8])
+
+#if "n" in full_name:
+#    print("gvxvdeba")
+
+#print(full_name[2:9])
+#print(full_name[-1])
+#print(full_name[5:1:-1])
+#print(full_name.upper())
+full_name2="GIORGI KAPANADZE"
+print(full_name2.lower())
+
+full_name3="   name    "
+print(full_name3.strip())
+
+name = "giorgi"
+
+print(name.replace("i", "a"))
+
+
